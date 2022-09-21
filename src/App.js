b8a80742448935e70dac8af663dbd5a8d579bad2
Andreia Vieira <andreiavieira2353@gmail.com>
2022-09-21 13:51:47 -0300

    Layout do Jogo

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import palavras from "./palavras"
+import "./styles/style.css"
+import forca0 from "./img/assets/forca0.png"
+import forca1 from "./img/assets/forca1.png"
+import forca2 from "./img/assets/forca2.png"
+import forca3 from "./img/assets/forca3.png"
+import forca4 from "./img/assets/forca4.png"
+import forca5 from "./img/assets/forca5.png"
+import forca6 from "./img/assets/forca6.png"
+
+
+
+export default function App(props){
+  
+
+    return (<main> 
+
+            <img className="gallow"src={forca6} alt ="gallow"/>
+       
+        
+    </main>
+       
+    )
+    
+}
